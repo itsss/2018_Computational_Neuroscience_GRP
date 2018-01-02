e8b3a5b25a0d437c29ea8a9773f77db6e9cb1a5e
Taewon Kang <itschool@itsc.kr>
2018-01-02 11:59:03 +0900

    그래프 캡션 삽입

--- basics_matlab.m
+++ basics_matlab.m
@@ -1,20 +1,23 @@
 %%
 for i = 1 : 10
-    disp(i); % print 1 ~ 10
+disp(i); % print 1 ~ 10
 end
+
 %%
 x = 0;
 for i = 1 : 10
-    x = x + i;
-    disp(x); % print 1 ~ 55
+x = x + i;
+disp(x); % print 1 ~ 55
 end
+
 %%
 x = 5;
-if(x < 10) 
-    disp(x-5);
+if(x < 10)
+disp(x-5);
 else
-    disp(x+5);
+disp(x+5);
 end % print 0
+
 %%
 1:10 % 1 2 3 4 5 6 7 8 9 10
 cumsum(1:10) % 1 3 6 10 15 21 28 36 45 55
@@ -24,3 +27,8 @@ t=[0:0.1:3.14*2]
 x = sin(t);
 size(t)
 plot(t,x) % print graph(figure)
+plot(t,x,'x')
+xlabel('Time')
+ylabel('sin(t)')
+title('My awesome graph')
+

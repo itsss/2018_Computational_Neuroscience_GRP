51c2333f16e7a1527427f910baae8f69ee9f0911
Taewon Kang <itschool@itsc.kr>
2018-01-04 13:31:48 +0900

    Supervised learn (Graph)

---
+++ train/deltaRuleTrain.m
@@ -0,0 +1,31 @@
+a=0.1;
+tol=0.01;
+nIts=100000;
+[nPat,nIn]=size(InPat);
+[nPat,nOut]=size(DesOut);
+V=rand(nOut,nIn)*2-1;
+maxErr=10;
+
+for c = 1 : nIts,
+    pIndx=ceil(rand*nPat);
+    d=DesOut(pIndx,:)';
+    x=InPat(pIndx,:)';
+    q=V*x;
+    y=1./(1+exp(-q));
+    dy=y.*(1-y);
+    e=d-y;
+    g=e.*dy;
+    deltaV=a*g*x';
+    V=V+deltaV;
+    if rem(c, 20) == 0,
+        Q = (V*InPat')';
+        Out = 1./(1+exp(-Q));
+        maxErr=sum(abs(DesOut-Out));
+        bar(Out);
+        ylim([0 1]);
+        drawnow;
+    end
+    if maxErr < tol,
+        break
+    end
+end
---
+++ train/supervised_learn_deltaruletrain.m
@@ -0,0 +1,9 @@
+InPat = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
+DesOut=[1;1;0;0];
+deltaRuleTrain;
+disp(Out);
+
+InPat = [1 0 1 0; 1 1 1 1; 1 1 1 0; 1 0 0 1];
+DesOut=[1;1;0;0];
+deltaRuleTrain;
+disp(Out);

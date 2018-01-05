3c79542157ab31d5c51139c7d0418ddbecb4611e
Taewon Kang <itschool@itsc.kr>
2018-01-05 10:29:41 +0900

    without training

--- train/supervised_learn_deltaruletrain.m
+++ train/supervised_learn_deltaruletrain.m
@@ -1,9 +1,20 @@
-InPat = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
-DesOut=[1;1;0;0];
+% InPat = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
+% DesOut=[1;1;0;0];
+% deltaRuleTrain;
+% disp(Out);
+% 
+% InPat = [1 0 1 0; 1 1 1 1; 1 1 1 0; 1 0 0 1];
+% DesOut=[1;1;0;0];
+% deltaRuleTrain;
+% disp(Out);
+% 
+% InPat = [1 0 1 0; 1 1 1 1; 1 1 1 0; 1 0 0 1];
+% DesOut=[1;1;1;0];
+% deltaRuleTrain;
+% disp(Out);
+% 
+InPat = [1 1 1; 1 0 1; 0 1 1; 0 0 1];
+DesOut=[0;1;1;0];
 deltaRuleTrain;
-disp(Out);
+disp(out); %학습이 되지 않는 코드
 
-InPat = [1 0 1 0; 1 1 1 1; 1 1 1 0; 1 0 0 1];
-DesOut=[1;1;0;0];
-deltaRuleTrain;
-disp(Out);
